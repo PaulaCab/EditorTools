97a9fcaae8f2e2e418ed9d8ca7d75bdcdebfb999
PaulaCab <paula.cab.car@gmail.com>
2025-03-17 14:46:56 +0100

    [Add] Debug msg and notify

--- EditorTools/Plugins/ExtendEditor/Source/ExtendEditor/Public/DebugHelper.h
+++ EditorTools/Plugins/ExtendEditor/Source/ExtendEditor/Public/DebugHelper.h
@@ -1,5 +1,9 @@
 ﻿#pragma once
 
+#include "Misc/MessageDialog.h"
+#include "Widgets/Notifications/SNotificationList.h"
+#include "Framework/Notifications/NotificationManager.h"
+
 void Print(const FString& Message, const FColor& Color)
 {
     if(GEngine)
@@ -9,4 +13,24 @@ void Print(const FString& Message, const FColor& Color)
 void PrintLog(const FString& Message)
 {
     UE_LOG(LogTemp, Warning, TEXT("%s"), *Message);
+}
+
+EAppReturnType::Type ShowMsg(EAppMsgType::Type MsgType, const FString& Msg, bool bShowMsgAsWarning = true)
+{
+    if(bShowMsgAsWarning)
+    {
+        FText title = FText::FromString(TEXT("Warning"));
+        return FMessageDialog::Open(MsgType, FText::FromString(Msg), &title);
+    }
+
+    return FMessageDialog::Open(MsgType, FText::FromString(Msg));
+}
+
+void ShowNotify(const FString& Msg)
+{
+    FNotificationInfo NotifyInfo(FText::FromString(Msg));
+    NotifyInfo.bUseLargeFont = true;
+    NotifyInfo.FadeOutDuration = 5.f;
+
+    FSlateNotificationManager::Get().AddNotification(NotifyInfo);
 }
